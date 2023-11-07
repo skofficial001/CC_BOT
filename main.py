5eabe4672283f946b3f80cbc3fa085e5cc686227
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-07 18:20:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def main():
   bot = Client("my_bot",
                api_id="27649783",
                api_hash="834fd6015b50b781e0f8a41876ca95c8",
-               bot_token="6576720076:AAFyP-lc8DNZZeJG6k2ukC2om_yMUlZeJ4g",
+               bot_token="6576720076:AAENXvK1uqYNngQs10Yk38Z4b4IxFo4Mikw",
                plugins=plugins)
   clients = [user, bot]
   bot.set_parse_mode(enums.ParseMode.HTML)
