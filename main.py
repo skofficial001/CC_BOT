1f00d7b63c2b91f7b8caa514cf78bf1bbb2d917d
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-07 19:17:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ plugins = dict(root="plugins")
 async def main():
   user = Client("scrapper",
                 api_id="27649783",
-                api_hash="834fd6015b50b781e0f8a41876ca95c8")
+                api_hash="834fd6015b50b781e0f8a41876ca95c8"
+                owner_id="6691393517"
   bot = Client("my_bot",
                api_id="27649783",
                api_hash="834fd6015b50b781e0f8a41876ca95c8",
